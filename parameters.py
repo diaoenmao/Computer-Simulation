f5af43f52dc3727e4d0360c2fe149b60753d7597
Haoli Du <haolidu@ipsec-143-215-26-152.vpn.gatech.edu>
2016-03-04 23:19:09 -0500

    node capacity setup

--- parameters.py
+++ parameters.py
@@ -1,4 +1,9 @@
+import math
 DEBUG = 1
 PARKING_NODE_COLOR = 'b'
 EXIT_NODE_COLOR = 'r'
 STREET_NODE_COLOR = 'g'
+
+#Simulation parameter
+UNIT_LENGTH = math.ceil(5000/738)   #ft
+AVERAGE_CAR_SPACE_LENGTH = 16 #ft
