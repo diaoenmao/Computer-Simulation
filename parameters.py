cbcc1ce001d3a6fed96e5db7b82eb2e7f0057529
Haoli Du <haolidu@lawn-128-61-75-29.lawn.gatech.edu>
2016-04-07 14:45:26 -0400

    init simulation parameters

---
+++ parameters.py
@@ -0,0 +1,10 @@
+class Parameter:
+    pass
+
+parameters = Parameter()
+parameters.blood_density = 1.05 #g/cm^3
+parameters.viscosity = 0.04 #poise
+parameters.wall_viscoelasticity = 15 #degree
+parameters.poission_ratio = 0.5 
+parmaeters.nominal_reflection_coefficient = 0.8
+parameters.artery_terminal_time = 1 #?
